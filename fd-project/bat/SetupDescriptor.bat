f591e2d78dbcb290e835ff81ac3454c4edcc046e
ManJav <mansurjavadi@gmail.com>
2018-03-27 17:52:47 +0430

    moved discriptor template to obj changed permission template

--- fd-project/bat/SetupDescriptor.bat
+++ fd-project/bat/SetupDescriptor.bat
@@ -1,14 +1,16 @@
-set APP_XML_TEMP=application-template.xml
+set APP_XML_TEMP=obj\application-template.xml
 set APP_XML=application.xml
 set DESC_TEMP=__DESCRIPTION__
 set DESC_FINE={"platform":"%PLATFORM%", "market":"%MARKET%"}
 
-set PERMISSION_TEMP=uses-permission-market
-set PERMISSION_FINE=uses-permission-market
-if %MARKET%==cafebazaar	set PERMISSION_FINE=uses-permission android:name="com.farsitel.bazaar.permission.PAY_THROUGH_BAZAAR"
-if %MARKET%==google		set PERMISSION_FINE=uses-permission android:name="com.android.vending.BILLING"
-if %MARKET%==myket		set PERMISSION_FINE=uses-permission android:name="ir.mservices.market.BILLING"
-if %MARKET%==ario		set PERMISSION_FINE=uses-permission android:name="com.arioclub.android.sdk.IAB"
+set PERMISSION_TEMP=com.domain.market.BILLING
+set PERMISSION_FINE=com.domain.market.BILLING
+if %MARKET%==cafebazaar	set PERMISSION_FINE=com.farsitel.bazaar.permission.PAY_THROUGH_BAZAAR
+if %MARKET%==google		set PERMISSION_FINE=com.android.vending.BILLING
+if %MARKET%==myket		set PERMISSION_FINE=ir.mservices.market.BILLING
+if %MARKET%==ario		set PERMISSION_FINE=com.arioclub.android.sdk.IAB
+if %MARKET%==cando		set PERMISSION_FINE=com.ada.market.BILLING
+
 
 ::echo %MARKET%...%PLATFORM%...%PERMISSION_FINE%
 
