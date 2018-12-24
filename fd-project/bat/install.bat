ac3e44410bf4d52b35c468b3e749b346a4c387fb
ManJav\Yoga <mansurjavadi@gmail.com>
2018-12-24 21:35:07 +0330

    fixed install bat bugs

--- fd-project/bat/install.bat
+++ fd-project/bat/install.bat
@@ -5,8 +5,8 @@ set APP_XML=application.xml
 for /f "tokens=3 delims=<>" %%a in ('findstr /R /C:"^[ 	]*<id>" %APP_XML%') do set APP_ID=%%a
 set APP_ID=%APP_ID: =%
 
-"C:\_projects\4.6.0+26.0.0\lib\android\bin\adb" devices
+"C:\_projects\4.6.0+31.0.0\lib\android\bin\adb" devices
 set /p APK_FILE="Drag APK: "
-"C:\_projects\4.6.0+26.0.0\lib\android\bin\adb" -d install -r "%APK_FILE%"
-"C:\_projects\4.6.0+26.0.0\lib\android\bin\adb" shell am start -n air.%APP_ID%/.AppEntry
+"C:\_projects\4.6.0+31.0.0\lib\android\bin\adb" -d install -r "%APK_FILE%"
+"C:\_projects\4.6.0+31.0.0\lib\android\bin\adb" shell am start -n air.%APP_ID%/.AppEntry
 pause
