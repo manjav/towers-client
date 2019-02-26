b11f7a4fa62ef3940620db30ed91184ff9acdcaf
Mansour <mansurjavadi@gmail.com>
2019-02-26 12:30:01 +0330

    added ios certificates

--- fd-project/bat/SetupApp.bat
+++ fd-project/bat/SetupApp.bat
@@ -17,10 +17,10 @@ set AND_ICONS=icons/android
 set AND_SIGNING_OPTIONS=-storetype pkcs12 -keystore "%AND_CERT_FILE%" -storepass %AND_CERT_PASS%
 
 :: iOS packaging
-set IOS_DIST_CERT_FILE=
-set IOS_DEV_CERT_FILE=
-set IOS_DEV_CERT_PASS=
-set IOS_PROVISION=cert\towerstory.mobileprovision
+set IOS_DIST_CERT_FILE=cert\TOD-Distribution-Certificate.p12
+set IOS_DEV_CERT_FILE=cert\TOD-Distribution-Certificate.p12
+set IOS_DEV_CERT_PASS=pppppp
+set IOS_PROVISION=cert\Bluebox_K2K_Adhoc_Profile.mobileprovision
 set IOS_ICONS=icons/ios
 
 set IOS_DEV_SIGNING_OPTIONS=-storetype pkcs12 -keystore "%IOS_DEV_CERT_FILE%" -storepass %IOS_DEV_CERT_PASS% -provisioning-profile %IOS_PROVISION%
